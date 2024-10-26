4a2bb7237f16196a3d36d1dd68a7e067f31f4217
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-10-26 17:47:45 +0500

    Task3

---
+++ Tyuiu.KonevaDD.Sprint3.Task3.V16/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KonevaDD.Sprint3.Task3.V16.Lib;
+namespace Tyuiu.KonevaDD.Sprint3.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = "ice nice ice creamcc";
+            char chr = 'c';
+            Console.WriteLine("Исходная строка  = " + value);
+            Console.WriteLine("Искомый символ = " + chr);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+
+
+
+    }
+    
+        
+
+
+
+
+
+
+
+
+ } 
