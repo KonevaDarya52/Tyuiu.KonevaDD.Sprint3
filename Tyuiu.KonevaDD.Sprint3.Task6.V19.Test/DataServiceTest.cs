247be386d600452e8160c48e73b8f3629e9b9f60
Дарья Конева <Tyuiu.KonevaDD@outlook.com>
2024-10-28 19:28:16 +0500

    Task6

--- Tyuiu.KonevaDD.Sprint3.Task6.V19.Test/DataServiceTest.cs
+++ Tyuiu.KonevaDD.Sprint3.Task6.V19.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KonevaDD.Sprint3.Task6.V19.Test
             int startValue = 10;
             int stopValue = 15;
             int result = ds.GetSumTheDivisors(startValue, stopValue);
-            int expected = 45;
+            int expected = 16;
             Assert.AreEqual(expected, result);
         }
     }
